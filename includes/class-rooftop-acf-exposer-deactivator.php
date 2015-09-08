6efc2691442ece5430a7449f5a95ca175b356fa1
Paul Hendrick <paul@errorstudio.co.uk>
2015-09-08 11:41:32 +0100

    update composer.json description

---
+++ includes/class-rooftop-acf-exposer-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://errorstudio.co.uk
+ * @since      1.0.0
+ *
+ * @package    Rooftop_Acf_Exposer
+ * @subpackage Rooftop_Acf_Exposer/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Rooftop_Acf_Exposer
+ * @subpackage Rooftop_Acf_Exposer/includes
+ * @author     Error <info@errorstudio.co.uk>
+ */
+class Rooftop_Acf_Exposer_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
