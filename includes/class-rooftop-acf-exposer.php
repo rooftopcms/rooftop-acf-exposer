39579344d6c20eca435b001895caebe8d4cbf8f8
Paul Hendrick <paul@errorstudio.co.uk>
2015-10-30 16:57:24 +0000

    refactor hooks to use rest_prepare_$type to add the ACF fields to the content['advanced'] attribute in the response

--- includes/class-rooftop-acf-exposer.php
+++ includes/class-rooftop-acf-exposer.php
@@ -171,7 +171,7 @@ class Rooftop_Acf_Exposer {
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
-        $this->loader->add_filter( 'rest_api_init', $plugin_public, 'add_acf_fields_to_content', 10, 3 );
+        $this->loader->add_action( 'rest_api_init', $plugin_public, 'prepare_acf_hooks', 10 );
         $this->loader->add_filter( 'rooftop_acf_field_value', $plugin_public, 'get_acf_field_value', 10, 2 );
 	}
 
